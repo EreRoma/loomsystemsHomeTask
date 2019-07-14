0a962ae4c984a1d4c2feb50aec4759afea518e1b
RomaE <ere.roma@gmail.com>
2019-07-14 22:10:51 +0300

    addWritingToErrorFile

--- src/com/loomsystems/logs/service/ILogsProcessor.java
+++ src/com/loomsystems/logs/service/ILogsProcessor.java
@@ -3,6 +3,7 @@ package com.loomsystems.logs.service;
 import java.util.List;
 
 public interface ILogsProcessor {
+
     void processLine(String logLine);
     List<String> getResults();
     List<String> getWrongFormatLines();
